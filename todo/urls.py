fc907787d33dd081109ccc849cc3451e4b0d94ad
Martin010 <martin.serret47@gmail.com>
2023-06-15 14:42:14 +0200

    - Edit button - Delete button - Completed task list

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,8 @@ from todo import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    path('mark-as-done/<int:pk>/', views.mark_as_done, name='mark-as-done'),
+    path('mark-as-undone/<int:pk>/', views.mark_as_undone, name='mark-as-undone'),
+    path('edit-task/<int:pk>/', views.edit_task, name='edit-task'),
+    path('delete-task/<int:pk>/', views.delete_task, name='delete-task'),
 ]
